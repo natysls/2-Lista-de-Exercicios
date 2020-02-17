6bffdae2b37c314f71b09d9a3589c97637b7f2fa
natysls <59856222+natysls@users.noreply.github.com>
2020-02-17 20:10:16 -0300

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,10 @@
+
+public class Main1 {
+    public static void main(String[] args) {
+        Pais p1 = new Pais("Brasil", "Brasilia", 700);
+        
+        p1.capEPaises();
+//        p1.equals(Brasil);
+    }
+    
+}
